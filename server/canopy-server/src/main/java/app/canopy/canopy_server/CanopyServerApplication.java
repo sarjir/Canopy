34972a3ade96c05196c4b523b1b4ce5b7ca0aef9
Sara Kent <sara@kentkodar.nu>
2026-06-29 22:28:50 +0200

    scaffold springboot

---
+++ server/canopy-server/src/main/java/app/canopy/canopy_server/CanopyServerApplication.java
@@ -0,0 +1,13 @@
+package app.canopy.canopy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanopyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanopyServerApplication.class, args);
+	}
+
+}
---
+++ server/canopy-server/src/test/java/app/canopy/canopy_server/CanopyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.canopy.canopy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanopyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
