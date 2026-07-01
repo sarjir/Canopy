6ff6063b44896c92e3dc578518e6eb5aff773417
Sara Kent <sara@kentkodar.nu>
2026-07-01 09:42:17 +0200

    Add first working request

--- server/src/main/java/app/canopy/canopy_server/CanopyServerApplication.java
+++ server/src/main/java/app/canopy/canopy_server/CanopyServerApplication.java
@@ -2,9 +2,19 @@ package app.canopy.canopy_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CanopyServerApplication {
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CanopyServerApplication.class, args);
